cc85cf7a15311ea4909f85fc33463031851244b9
Cade Ward <cadew@live.com>
2014-08-22 16:07:55 -0600

    phantomjs

--- Gruntfile.js
+++ Gruntfile.js
@@ -558,7 +558,7 @@ module.exports = function ( grunt ) {
     'karma:continuous'
   ]);
 
-  grunt.registerTask( 'test', [
+  grunt.registerTask( 'setup', [
     'clean', 'html2js', 'jshint', 'coffeelint', 'coffee', 'less:build',
     'concat:build_css', 'copy:build_app_assets', 'copy:build_vendor_assets',
     'copy:build_appjs', 'copy:build_vendorjs', 'index:build'
